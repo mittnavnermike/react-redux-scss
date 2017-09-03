5f95d18761f1639ae345909e87d3b6d0670243aa
Mike Patterson <mikepatterson@Mikes-MacBook-Pro.local>
2017-09-03 20:55:09 +0200

    :sparkles: set app state to localStorage

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,15 +1,18 @@
 import deepFreeze from 'deep-freeze-es6';
 
+const setLocal = (state) => {
+    window.localStorage.setItem('app', JSON.stringify(state));
+}
+
 function app(state = deepFreeze([]), action) {
     switch (action.type) {
 
     case 'LOGG_IN': {
         console.log(`User is ${action.bool ? 'logged in' : 'not logged in'}`);
-
         const newState = Object.assign({}, state, {
             logged_in: action.bool,
         });
-
+        setLocal(newState);
         return newState;
     }
     default:
